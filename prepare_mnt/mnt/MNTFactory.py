0e436dd73ce6947fdc8eb1f6bda532dc299f61d6
Peter KETTIG <peter.kettig@cnes.fr>
2020-05-27 23:31:31 +0200

    Updated logger

--- prepare_mnt/mnt/MNTFactory.py
+++ prepare_mnt/mnt/MNTFactory.py
@@ -8,15 +8,17 @@ file 'LICENSE.md', which is part of this source code package.
 Author:         Peter KETTIG <peter.kettig@cnes.fr>
 """
 
+import logging
 from prepare_mnt.mnt.SRTM import SRTM
 from prepare_mnt.mnt.EuDEM import EuDEM
+
 
 class MNTFactory:
     """
     Create a given DEM in Maja format
     """
     def __init__(self, site, platform_id, mission_field, mnt_resolutions, **kwargs):
-        self.type_dem = kwargs.get("type_dem", "srtm").lower()
+        self.type_dem = kwargs.get("type_dem", "all").lower()
         self.site = site
         self.plaform_id = platform_id
         self.mission_field = mission_field
@@ -30,22 +32,43 @@ class MNTFactory:
         Checks the given mnt_type and returns the class accordingly
         :return: A derived MNTBase object, None if mnt_type unknown.
         """
+        error = None
         # TODO Add more options here: ALOS, TDX...
-        if self.type_dem == "srtm":
+
+        if self.type_dem in ["eudem", "all"]:
+            try:
+                return EuDEM(site=self.site,
+                             **self.kwargs).to_maja_format(platform_id=self.plaform_id,
+                                                           mission_field=self.mission_field,
+                                                           mnt_resolutions=self.mnt_resolutions,
+                                                           coarse_res=self.coarse_res,
+                                                           full_res_only=self.full_res_only)
+            except Exception as e:
+                error = e
+                logger.error(e)
+
+        if self.type_dem in ["srtm", "all"]:
             # SRTM is distributed in 90m.
             # Thus, all initial calculation has to be done at this resolution:
-            self.site.res_x, self.site.res_y = 90, 90
-            return SRTM(site=self.site,
-                        **self.kwargs).to_maja_format(platform_id=self.plaform_id,
-                                                      mission_field=self.mission_field,
-                                                      mnt_resolutions=self.mnt_resolutions,
-                                                      coarse_res=self.coarse_res,
-                                                      full_res_only=self.full_res_only)
-        elif self.type_dem == "eudem":
-            return EuDEM(site=self.site,
-                         **self.kwargs).to_maja_format(platform_id=self.plaform_id,
-                                                       mission_field=self.mission_field,
-                                                       mnt_resolutions=self.mnt_resolutions,
-                                                       coarse_res=self.coarse_res,
-                                                       full_res_only=self.full_res_only)
-        return None
+            self.site.res_x, self.site.res_y = 90, -90
+            try:
+                return SRTM(site=self.site,
+                            **self.kwargs).to_maja_format(platform_id=self.plaform_id,
+                                                          mission_field=self.mission_field,
+                                                          mnt_resolutions=self.mnt_resolutions,
+                                                          coarse_res=self.coarse_res,
+                                                          full_res_only=self.full_res_only)
+            except ValueError as e:
+                error = e
+                logger.error(e)
+
+        if not error:
+            raise ValueError("No DEM type set. Please select 'eudem', 'srtm' or 'all'.")
+
+        raise error
+
+
+if __name__ == "__main__":
+    pass
+else:
+    logger = logging.getLogger("root")

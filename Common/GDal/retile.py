89cd7ac8d0b2ca6e1df579aec55a99843fbc1713
Peter KETTIG <peter.kettig@cnes.fr>
2020-06-14 19:07:37 +0200

    Updated Headers on all files

--- Common/GDal/retile.py
+++ Common/GDal/retile.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2016-2020 Centre National d'Etudes Spatiales (CNES), CSSI, CESBIO  All Rights Reserved
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 ###############################################################################
 #  $Id: gdal_retile.py f894c441f4b5830a9801bfb3dda556b1baa7ee9f 2019-02-22 12:37:35Z DanielEvans $
 #
